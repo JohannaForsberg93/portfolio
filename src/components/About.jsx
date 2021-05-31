b56805a57b9c77f539e398950ae0ca093ab28564
JohannaForsberg93 <johanna_forsberg@msn.com>
2021-05-31 23:52:12 +0200

    Testat olika bakgrundsbilder

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import stylesheet from '../styling/About.module.css';
+import portrait from '../styling/hej.jpg';
 // import test from '../styling/linkedin.svg';
 
 function About() {
@@ -10,16 +11,20 @@ function About() {
 				<h1>VEM ÄR JAG?</h1>
 				<div className={stylesheet.content}>
 					<div className={stylesheet.grid1}>
-						<p>Mitt namn är Johanna och jag har precis gått en 2-årig utbildning inom frontend på EC Utbildning i Göteborg. I utbildningen ingick en 24 veckors lång LIA där jag fick vara ute på arbetsplats och skaffa mig värdefull kunskap och erfarenhet från arbetslivet. Jag tog min examen den 18:e maj 2021 och är nu redo för jobb. <br></br><br />
+						<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br /><br />
+
+							Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+
+						{/* <p>Mitt namn är Johanna och jag har precis gått en 2-årig utbildning inom frontend på EC Utbildning i Göteborg. I utbildningen ingick en 24 veckors lång LIA där jag fick vara ute på arbetsplats och skaffa mig värdefull kunskap och erfarenhet från arbetslivet. Jag tog min examen den 18:e maj 2021 och är nu redo för jobb. <br></br><br />
 
 						Just nu söker jag mitt första jobb som frontendutvecklare samtidigt som jag arbetar på min portfolio. Jag jobbar även i kassan på ICA sen 5 år tillbaka men söker aktivt efter andra jobb.  <br></br><br />
 
 						Förutom att koda och vara kreativ så gillar jag att baka och mysa med mina två katter Tessan och Lilo.
-						</p>
+						</p> */}
 					</div>
 
 					<div className={stylesheet.grid2}>
-						<img src="https://cdn.shopify.com/s/files/1/0148/6268/0118/products/marmeladkulor_2048x.jpg?v=1617300005" alt="gooodis" width="500" height="500" />
+						<img src={portrait} alt={"Profile picture"} width="550" height="500" />
 					</div>
 				</div>
 				{/* <h3>Vill du veta mer om mig? Hör av dig till mig!</h3>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,8 +5,32 @@ function Projects() {
 	return (
 		<div className={stylesheet.container}>
 			<div className={stylesheet.section}>
-				<div className={stylesheet.content}>
-					<h1>This is Project page</h1>
+				<h1>Mina projekt från skolan</h1>
+				<div className={stylesheet.grid}>
+					<div className={stylesheet.card}>
+						<h5>Code snippets</h5>
+						<p>Testing testing</p>
+					</div>
+					<div className={stylesheet.card}>
+						<h5>Saloon</h5>
+						<p>Testing testing</p>
+					</div>
+					<div className={stylesheet.card}>
+						<h5>Bakbloggen</h5>
+						<p>Testing testing</p>
+					</div>
+					<div className={stylesheet.card}>
+						<h5>Star Wars</h5>
+						<p>Testing testing</p>
+					</div>
+					<div className={stylesheet.card}>
+						<h5>I love movies</h5>
+						<p>Testing testing</p>
+					</div>
+					<div className={stylesheet.card}>
+						<h5>La coffee</h5>
+						<p>Testing testing</p>
+					</div>
 				</div>
 			</div>
 		</div>
